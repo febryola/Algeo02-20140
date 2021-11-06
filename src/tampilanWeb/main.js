c0c3cb53b98e6f01253a706a81941680a12ec282
Steven Siahaan <stevensiahaan25@gmail.com>
2021-11-06 17:02:45 +0700

    upload image ui compression patch

--- src/tampilanWeb/main.js
+++ src/tampilanWeb/main.js
@@ -7,7 +7,37 @@ const apiUrl = "http://localhost:8080/compress";
  */
 const fileInput = document.getElementById(fileInputId);
 const submitButton = document.getElementById(submitButtonId);
+const fileName = document.querySelector(".file-name");
+const wrapper = document.querySelector(".wrapper");
+const defaultBtn = document.querySelector("#file");
+const customBtn = document.querySelector("#custom-btn");
+const cancelBtn = document.querySelector("#cancel-btn i");
+const img = document.querySelector("img");
+let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
 
+function defaultBtnActive() {
+    defaultBtn.click();
+}
+defaultBtn.addEventListener("change", function() {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function() {
+            const result = reader.result;
+            img.src = result;
+            wrapper.classList.add("active");
+        }
+        cancelBtn.addEventListener("click", function() {
+            img.src = "";
+            wrapper.classList.remove("active");
+        })
+        reader.readAsDataURL(file);
+    }
+    if (this.value) {
+        let valueStore = this.value.match(regExp);
+        fileName.textContent = valueStore;
+    }
+});
 submitButton.addEventListener("click", () => {
   const file = fileInput.files[0];
   const formData = new FormData();
